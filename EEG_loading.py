390d0483a3bd08645b88b334b99eaa9f965e81f4
npiro <nicolas.piro@gmail.com>
2016-10-05 16:27:15 +0200

    erased useless files

--- EEG_loading.py
+++ EEG_loading.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[36]:
 
 get_ipython().magic('matplotlib inline')
 import scipy.io
@@ -10,51 +10,30 @@ import glob
 from matplotlib.pyplot import *
 import seaborn
 import numpy as np
-folder = 'train_1/'
+folder = '../Data/train_1/'
 files = glob.glob(join(folder,'1_*_1.mat'))
 
 
-# In[2]:
+# In[37]:
 
-import re
-pattern = '1_(\w+?)_1'
-strings = ['1_16_1.mat','1_15_1.mat']
-result = re.match(pattern, strings[0])
-result.group()
+sortedfiles = sorted(files, key=lambda name: int(name[18:-6]))
 
 
-# In[3]:
+# In[39]:
 
 data = []
-for i, f in enumerate(files):
+for i, f in enumerate(sortedfiles):
+    if i > 5: break
     mat = scipy.io.loadmat(f)
     dataStruct=mat['dataStruct']
     data.extend(dataStruct['data'][0,0].tolist())
-    if i >= 9: break
+    
 data = np.array(data)
 
 
-# In[2]:
+# In[40]:
 
-files
-
-
-# In[6]:
-
-data = dataStruct['data'][0,0]
-
-
-# In[7]:
-
-d = []
-d.extend(data.tolist())
-d.extend(data.tolist())
-np.array(d).shape
-
-
-# In[8]:
-
-plot(data[:,10])
+plot(data[:,0])
 
 
 # In[ ]:
