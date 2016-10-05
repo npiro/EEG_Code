5cd7dcc0fc3d8974ed8d3666570e6dad30fef71b
npiro <nicolas.piro@gmail.com>
2016-10-05 16:26:26 +0200

    cleaned up

---
+++ EEG_loading-3.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[36]:
+
+get_ipython().magic('matplotlib inline')
+import scipy.io
+from os.path import join
+import glob
+from matplotlib.pyplot import *
+import seaborn
+import numpy as np
+folder = '../Data/train_1/'
+files = glob.glob(join(folder,'1_*_1.mat'))
+
+
+# In[37]:
+
+sortedfiles = sorted(files, key=lambda name: int(name[18:-6]))
+
+
+# In[39]:
+
+data = []
+for i, f in enumerate(sortedfiles):
+    if i > 5: break
+    mat = scipy.io.loadmat(f)
+    dataStruct=mat['dataStruct']
+    data.extend(dataStruct['data'][0,0].tolist())
+    
+data = np.array(data)
+
+
+# In[40]:
+
+plot(data[:,0])
+
+
+# In[ ]:
+
+
+
