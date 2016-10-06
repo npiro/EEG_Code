86165c2388372bc18ae9f7f5d52fd3f9fbbe35ae
npiro <nicolas.piro@gmail.com>
2016-10-06 15:02:19 +0200

    comments fixed

--- plot_sample.py
+++ plot_sample.py
@@ -7,7 +7,8 @@ Created on Wed Oct  5 17:53:07 2016
 # In[0]:
 from matplotlib.pyplot import *
 import pandas as pd
-
+from load_data import load_trace
+from scipy import signal
 # In[1]:
 dataNormal = load_trace(state='normal')
 dataPreictal = load_trace(state='preictal')
@@ -17,11 +18,18 @@ dataPreictal = load_trace(state='preictal')
 chan = [3]
 ax = gca()
 ax.clear()
-avwin = 100
+avwin = 1
 dfN = pd.DataFrame(dataNormal[:,:])
 dfP = pd.DataFrame(dataPreictal[:,:])
-plot(pd.rolling_mean(dfN.iloc[:100000,chan], avwin))
-plot(pd.rolling_mean(dfP.iloc[:100000,chan], avwin))
+plot(pd.rolling_mean(dfN.iloc[:100000,chan], avwin),label='normal')
+plot(pd.rolling_mean(dfP.iloc[:100000,chan], avwin),label='preictal')
+ax.legend()
+
+
+# In[3]:
+ax.clear()
+f, Pxx_den = signal.periodogram(dfN, 10)
+semilogy(f, Pxx_den)
 
 # In[3]:
 ax = gca()
